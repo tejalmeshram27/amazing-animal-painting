0e7d3cb038c0532d17d44fbed94297b4af33c7ec
tejalmeshram27 <136323056+tejalmeshram27@users.noreply.github.com>
2024-05-31 12:18:06 +0530

    First Commit
    First Date: - 31-05-2024

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ProductModule } from './product/product.module';
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+import { CartModule } from './cart/cart.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ProductModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    CartModule,
+  ],
+  providers: [provideClientHydration(), provideAnimationsAsync()],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
